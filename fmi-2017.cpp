6784ec89ecd5b7ed489d9f79ddaf4523931f370a
alexC2K <50263900+alexC2K@users.noreply.github.com>
2019-05-04 13:57:18 +0300

    Schimbat numele fisierului.

---
+++ fmi-2017.cpp
@@ -0,0 +1,33 @@
+// Problema a
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+ifstream f("date.in");
+int L;
+char s[10000];
+
+
+int main() {
+    f >> L;
+    f.get();
+    f.get(s, 10000);
+
+    int counting = 0;
+    char *p = strtok(s, " ");
+    while(p != NULL) {
+        counting += strlen(p);
+        if(counting <= L) {
+            cout << p << " ";
+            counting++;
+        }
+        else {
+            cout << "\n";
+            counting = 0;
+            counting += strlen(p);
+            cout << p << " ";
+        }
+        p = strtok(NULL, " ");
+    }
+}
